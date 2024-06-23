40dfb393e06bf6968acca3a9373ec4f7063c6873
nguyenhiepvn <nguyenvanhiepkenshu@gmail.com>
2024-06-23 18:18:30 +0900

    ルートを変更したい場合は、HTTPリクエストのヘッダーにCSRFトークンを追加する必要があります。

--- app/javascript/memo.js
+++ app/javascript/memo.js
@@ -1,14 +1,40 @@
-﻿function post (){
+﻿const buildHTML = (XHR) => {
+    const item = XHR.response.post;
+    const html = `
+      <div class="post">
+        <div class="post-date">
+          投稿日時：${item.created_at}
+        </div>
+        <div class="post-content">
+          ${item.content}
+        </div>
+      </div>`;
+    return html;
+  };
+
+function post (){
     console.log("イベント発火")
     const form = document.getElementById("form")
-    form.addEventListener("submit",(e) => {
-        e.preventDefault();
-        const formData = new FormData(form);
-        const XHR = new XMLHttpRequest();
-        XHR.open("POST","posts", true);
-        XHR.responseType = "json";
-        XHR.send(formData)
-    })
+    form.addEventListener
+    ("submit",(e) => 
+        {
+          const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+          console.log(csrfToken); 
+            e.preventDefault();
+            const formData = new FormData(form);
+            const XHR = new XMLHttpRequest();
+            
+            XHR.open("POST","posts123", true);
+            // XHR.setRequestHeader('X-CSRF-Token', csrfToken);
+
+            XHR.responseType = "json";
+            XHR.send(formData)
+            XHR.onload = () => 
+            {
+                list.insertAdjacentHTML("afterend", buildHTML(XHR));
+            };
+        }
+    )
   };
 
 window.addEventListener('turbo:load', post);
